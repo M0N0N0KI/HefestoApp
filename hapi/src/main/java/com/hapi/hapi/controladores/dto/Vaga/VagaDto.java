adbd7e31f07156f131ac6cbfd5c8bb55e42b5dcb
Gilson <gilsonmartins124@gmail.com>
2022-06-13 21:40:51 -0400

    :rocket: criação da VagaDto

---
+++ hapi/src/main/java/com/hapi/hapi/controladores/dto/Vaga/VagaDto.java
@@ -0,0 +1,16 @@
+package com.hapi.hapi.controladores.dto.Vaga;
+
+import java.sql.Date;
+
+public class VagaDto {
+    private Long id;
+    private String cargaHoraria;
+    private String status;
+    private Long empresa;
+    private String preRequisitos;
+    private String diferenciais;
+    private String descricao;
+    private Date inicio;
+    private Date fim;
+
+}
