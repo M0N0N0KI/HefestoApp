aa63b91dc67ffa6a0aa4de18365cda9ed99510ed
RhuandaSilvaReis <rhuandasilvareis@gmail.com>
2022-06-21 11:28:44 -0400

    Criado o modelo Colaborador e Empresa

---
+++ hapi/src/main/java/com/hapi/hapi/modelos/Colaborador/Colaborador.java
@@ -0,0 +1,36 @@
+package com.hapi.hapi.modelos.Colaborador;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "colaborador")
+public class Colaborador implements Serializable
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contato_id")
+    private Long id;
+
+    @Column(name ="contato_email", length = 120)
+    private String email;
+
+    @Column(name = "contato_telefone", length = 12)
+    private String telefone;
+
+    @Column(name ="contato_celular", length = 120)
+    private String celular;
