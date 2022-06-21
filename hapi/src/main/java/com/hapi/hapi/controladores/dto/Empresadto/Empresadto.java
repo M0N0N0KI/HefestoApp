849331b2ca9bacb79a81cc598a8f787939ec4745
RhuandaSilvaReis <rhuandasilvareis@gmail.com>
2022-06-21 15:30:49 -0400

    Criação do empresadto

---
+++ hapi/src/main/java/com/hapi/hapi/controladores/dto/Empresadto/Empresadto.java
@@ -0,0 +1,16 @@
+package com.hapi.hapi.controladores.dto.Empresadto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Empresadto {
+    
+    private Long id;
+    private String site;
+    private String tipo;
+    
+     
+}
